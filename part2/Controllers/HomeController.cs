27a6b719a0b309b0c94302104e44e5ce48fed362
Arjun Kalburgi <askalburgi@gmail.com>
2018-01-03 14:25:29 -0500

    slideshow from json!!

--- part2/Controllers/HomeController.cs
+++ part2/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using part2.Models;
 
@@ -10,9 +11,17 @@ namespace part2.Controllers
 {
     public class HomeController : Controller
     {
+
+        private readonly IHostingEnvironment _appEnvironment;
+
+        public HomeController(IHostingEnvironment appEnvironment)
+        {
+            _appEnvironment = appEnvironment;
+        }
+
         public IActionResult Index()
         {
-            return View(new slideshow());
+            return View(new slideshow(_appEnvironment));
         }
 
         public IActionResult About()
--- part2/Models/slideshow.cs
+++ part2/Models/slideshow.cs
@@ -1,19 +1,37 @@
 ﻿using System;
+using System.Linq;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+
 namespace part2.Models
 {
     public class slideshow
     {
+        public static IConfigurationRoot Configuration { get; set; }
         public item[] slideshowitems { get; }
+        public string output { get;  }
 
-        public slideshow()
+        public slideshow(IHostingEnvironment env)
         {
-            // initiate slideshowitems
-            slideshowitems = new item[4];
-            slideshowitems[0] = new item("~/images/slideshow(0).jpg", "beginning of spring, snow melted but still looks gross"); 
-            slideshowitems[1] = new item("~/images/slideshow(1).jpg", "jumping on rocks in iceland"); 
-            slideshowitems[2] = new item("~/images/slideshow(2).jpg", "I think I lost those gloves in Iceland"); 
-            slideshowitems[3] = new item("~/images/slideshow(3).jpg", "testing my remote shooter camera feature"); 
-    
+            // get json
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.WebRootPath)
+                .AddJsonFile("lib/images.json"); 
+            Configuration = builder.Build();
+
+            // read json to array 
+            var someArray = Configuration
+                .GetSection("itemsarray")
+                .GetChildren()
+                .Select(x => new item(
+                    x.GetChildren().Last().Value,
+                    x.GetChildren().First().Value
+                    ))
+                .ToArray();
+
+            // set slideshowitems
+            this.slideshowitems = new item[someArray.Length];
+            this.slideshowitems = someArray; 
         }
     }
 
