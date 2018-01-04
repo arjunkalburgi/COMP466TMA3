6df418dba86dac2063bf88230bb056ab81bfa68f
Arjun Kalburgi <askalburgi@gmail.com>
2018-01-03 23:12:21 -0500

    part 3 set up

---
+++ part3/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace part3.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
